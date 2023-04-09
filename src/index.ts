57220ec08353205c44550497a88141ee68eefc99
Christopher Alba <calb200@aucklanduni.ac.nz>
2023-04-09 14:23:45 +1200

    setup swagger express typescript

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from "express";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+
+import Router from "./routes";
+
+const PORT = process.env.PORT || 8000;
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+
+app.use(
+  "/swagger",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
+
+app.use(Router);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
